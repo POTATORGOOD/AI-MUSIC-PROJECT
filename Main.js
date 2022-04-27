8ba4384822b989d11852752d20b3430091158d5d
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-04-26 19:09:26 -0500

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,5 +1,9 @@
 muffin = "";
 pokemon = "";
+LeftWristX = 0;
+LeftWristY = 0;
+RightWristX = 0;
+RightWristY = 0;
 
 function preload() {
     muffin = loadSound("Muffin.mp3");
@@ -12,8 +16,26 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        LeftWristX = results[0].pose.leftWrist.x;
+        LeftWristY = results[0].pose.leftWrist.y;
+        RightWristX = results[0].pose.rightWrist.x;
+        RightWristY = results[0].pose.rightWrist.y;
+        console.log("left wrist x = " + LeftWristX + "left wrist y = " + LeftWristY + "right wrist x = " + RightWristX + "right wrist y = " + RightWristY)
+    }
 }
